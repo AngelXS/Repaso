8169acfa8528349d1bfa8d0bdfe1413146b00642
LUIS ANGEL SANGUANO CARRERA <luis.sanguano01@epn.edu.ec>
2022-07-05 21:56:46 -0500

    a dormir

--- Ejercicio_2/funciones.js
+++ Ejercicio_2/funciones.js
@@ -5,7 +5,7 @@ a4 = {nombre: "Pepito", profesion: "Doctor", edad: 4};
 a5 = {nombre: "Martin", profesion: "Ingeniero", edad: 2};
 a6 = {nombre: "Juanito", profesion: "Doctor", edad: 23};
 a7 = {nombre: "Pepe", profesion: "Cantante", edad: 90};
-a8 = {nombre: "Matias", profesion: "Doctor", edad: 2};
+a8 = {nombre: "Matias", profesion: "Profesor", edad: 2};
 clientes = [a1,a2,a3,a4,a5,a6,a7,a8];
 
 var cargardatos = () => {
@@ -22,12 +22,15 @@ var buscar = (buscado) => {
     clientes.filter(element => {
         if(element.nombre == buscado){
             cadena=cadena+"<tr><td>"+element.nombre+"</td>"+"<td>"+element.profesion+"</td>"+"<td>"+element.edad+"</td></tr>";
+            document.getElementById("persona").innerText = "Hola soy "+element.nombre;
         }
+        
     });
     cadena =cadena+"<tr><th></th><th></th><th></th></tr>";
     cadena=cadena+"</table>";
     console.log(cadena);
     document.getElementById("tabla").innerHTML = cadena;
+   
 }
 var limpiar = () => document.getElementById("texto1").value=""; 
 
